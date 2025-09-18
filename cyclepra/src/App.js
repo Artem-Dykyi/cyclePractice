84955e18c0c00b8d9224b0a37322ddf8c9504efe
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2025-09-18 19:56:05 +0300

    firstChange

---
+++ cyclepra/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Component } from 'react';
+import Modal from './components/Modal';
+
+export class App extends Component{
+
+  render(){
+    return (
+      <div className="App">
+        <Modal/>
+      </div>
+    );
+  }
+
+}
+
+export default App;
